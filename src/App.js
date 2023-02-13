1a02089eab14b22ac916259b383cfda6bba43729
Hebert Richard Masseno Dias <hebertdev82@gmail.com>
2023-02-13 19:28:18 -0300

    construction

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Estamos construindo um novo site incrível! Em breve, estaremos de volta.
+        </p>
+        <a
+          className="App-link"
+          href="https://wa.me/5535997233493"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Chama a gente no WhatsApp!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
