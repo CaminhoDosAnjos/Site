4ff952c6f7425334a1f26eaeb819f8ca64b3af4b
Hebert Richard Masseno Dias <hebertdev82@gmail.com>
2023-03-27 01:25:23 -0300

    updating

--- src/App.js
+++ src/App.js
@@ -1,55 +1,23 @@
-import logo from './images/logo.png';
+import Container from 'react-bootstrap/Container';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        {/* <div className="Menu-Topo">
-          <ul className="Menu">
-            <li>Home</li>
-            <li>Fale Conosco</li>
-            <li>Parceiros</li>
-            <li>Entrar</li>
-            <li>Criar Conta</li>
-          </ul>
+    <div className="container-fluid vw-100 vh-100 d-flex align-items-center justify-content-center text-center under_construction">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Nosso site está em construção.</h1>
+          <h3>Siga-nos nas Redes Sociais e fique por dentro!</h3>
+
+          <a href="https://www.facebook.com/araucariaecoturpg" rel="noreferrer" target={'_blank'}><FontAwesomeIcon icon={brands('facebook')} /></a>
+          <a href="https://www.instagram.com/caminhodosanjosmg/" rel="noreferrer" target={'_blank'}><FontAwesomeIcon icon={brands('instagram')} /></a>
+          <a href="https://goo.gl/maps/Cty39K2UcL9Lr6Q59" rel="noreferrer" target={'_blank'}><FontAwesomeIcon icon={brands('google')} /></a>
+          <a href="https://www.youtube.com/channel/UCHZmJCtwPLat38yoGg_zwqQ" rel="noreferrer" target={'_blank'}><FontAwesomeIcon icon={brands('youtube')} /></a>
+
         </div>
-
-        <div className="Logotipo">
-          <img src={logo} className="App-logo" alt="logo" />
-          <input type="text" className="Search-Bar" placeholder="Do que você precisa?" />
-          <ul className="Redes-Sociais">
-            <li>Facebook</li>
-            <li>Instagram</li>
-            <li>Google</li>
-            <li>Twitter</li>
-          </ul>
-        </div> */}
-
-
-
-
-
-
-
-
-
-
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Estamos construindo um novo site incrível! Em breve, estaremos de volta.
-        </p>
-        <a
-          className="App-link"
-          href="https://wa.me/5535997233493"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Chama a gente no WhatsApp!
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
