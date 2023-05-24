f7c7fbca0abc3e96af3a5b1eaa4ed01fbe581cb8
Hebert <hebertdev82@gmail.com>
2023-05-24 19:43:01 -0300

    construction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import Container from 'react-bootstrap/Container';
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import React, { Fragment } from 'react';
+import axios from 'axios';
+import { Container, Row, Col } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="container-fluid vw-100 vh-100 d-flex align-items-center justify-content-center text-center under_construction">
-      <div className="row">
-        <div className="col-md-12">
-          <h1>Nosso site está em construção.</h1>
-          <h3>Siga-nos nas Redes Sociais e fique por dentro!</h3>
+import Left from './components/Left';
+import Right from './components/Right';
 
-          <a href="https://www.facebook.com/araucariaecoturpg" rel="noreferrer" target={'_blank'}><FontAwesomeIcon icon={brands('facebook')} /></a>
-          <a href="https://www.instagram.com/caminhodosanjosmg/" rel="noreferrer" target={'_blank'}><FontAwesomeIcon icon={brands('instagram')} /></a>
-          <a href="https://goo.gl/maps/Cty39K2UcL9Lr6Q59" rel="noreferrer" target={'_blank'}><FontAwesomeIcon icon={brands('google')} /></a>
-          <a href="https://www.youtube.com/channel/UCHZmJCtwPLat38yoGg_zwqQ" rel="noreferrer" target={'_blank'}><FontAwesomeIcon icon={brands('youtube')} /></a>
-
-        </div>
-      </div>
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    
+    <main>
+      <Container fluid>
+        <Row>
+        
+          <Col xs={{ order: 1 }} md={{ size: 6 }} className="m-0 p-0">
+            <Left />
+          </Col>
+          
+          <Col xs={{ order: 2 }} md={{ size: 6 }} className="m-0 p-0">
+            <Right />
+          </Col>
+          
+        </Row>
+      </Container>
+    </main>
+    
+  </Fragment>
+);
 
 export default App;
